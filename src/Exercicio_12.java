6237eb138fab0716b55c148a6664d33717e69114
Francismarads22 <111935117+Francismarads22@users.noreply.github.com>
2022-08-27 21:59:15 -0300

    Exercicio 12

---
+++ src/Exercicio_12.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Exercicio_12 {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+
+		System.out.println("EX12");
+	}
+}
